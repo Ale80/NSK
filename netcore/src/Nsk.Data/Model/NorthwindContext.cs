91e9d671131f2e2c05905b3b794aec7c019c8031
ale <silveraxale@alice.it>
2017-06-01 12:41:26 +0200

    changed SQL Server db(server DEV01, db Northwind)

--- netcore/src/Nsk.Data/Model/NorthwindContext.cs
+++ netcore/src/Nsk.Data/Model/NorthwindContext.cs
@@ -8,8 +8,9 @@ namespace Nsk.Data.Model
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+			//
             #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-            optionsBuilder.UseSqlServer(@"Server=.\SQLExpress;Database=Northwind;Trusted_Connection=True;");
+            optionsBuilder.UseSqlServer(@"Server=DEV01;Database=Northwind;Trusted_Connection=True;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
